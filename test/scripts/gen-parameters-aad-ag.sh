feb56b26c8df742e8c11900b133be851b1fb4e54
Jianguo Ma <jiangma@microsoft.com>
2020-06-04 01:17:54 +0800

    dd-1117300: add template validation tests for different service integration to pre-flight job (#50)
    * update az cli version
    
    * add 4 test cases for pre-flight
    
    * invoke preflight tests in workflow
    
    * use bash instead of sh
    
    * userName instead of gitUserName
    
    * renaming parameters to the latest
    
    * add return code
    
    * add error message to identify which test case failed for triage later
    
    * add test case for ag with invalid arguments
    
    * create keyvalult and secrets for ag test case
    
    * generate ssl certificate
    
    * update keyvault name
    
    * add the other 3 test cases
    
    * unique name for keyvault
    
    * keyvault name must begin with a letter
    
    * add comments
    
    * adjust execution sequence of preflight job
    
    * use "az deployment group" to replace the deprecated "az group deployment"

---
+++ test/scripts/gen-parameters-aad-ag.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+parametersPath=$1
+githubUserName=$2
+testbranchName=$3
+keyVaultName=$4
+keyVaultResourceGroup=$5
+keyVaultSSLCertDataSecretName=$6
+keyVaultSSLCertPasswordSecretName=$7
+
+cat <<EOF > ${parametersPath}
+{
+    "\$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {
+        "_artifactsLocation": {
+            "value": "https://raw.githubusercontent.com/${githubUserName}/arm-oraclelinux-wls-cluster/${testbranchName}/arm-oraclelinux-wls-cluster/src/main/arm/"
+        },
+        "_artifactsLocationSasToken": {
+            "value": ""
+        },
+        "aadsPortNumber": {
+            "value": "636"
+        },
+        "aadsPublicIP": {
+            "value": "GEN-UNIQUE"
+        },
+        "aadsServerHost": {
+            "value": "GEN-UNIQUE"
+        },
+        "adminPasswordOrKey": {
+            "value": "GEN-UNIQUE"
+        },
+        "adminUsername": {
+            "value": "GEN-UNIQUE"
+        },
+        "enableAAD": {
+            "value": true
+        },
+        "enableAppGateway": {
+            "value": true
+        },
+        "enableDB": {
+            "value": false
+        },
+        "keyVaultName": {
+            "value": "${keyVaultName}"
+        },
+        "keyVaultResourceGroup": {
+            "value": "${keyVaultResourceGroup}"
+        },
+        "keyVaultSSLCertDataSecretName": {
+            "value": "${keyVaultSSLCertDataSecretName}"
+        },
+        "keyVaultSSLCertPasswordSecretName": {
+            "value": "${keyVaultSSLCertPasswordSecretName}"
+        },
+        "numberOfInstances": {
+            "value": 4
+        },
+        "wlsLDAPGroupBaseDN": {
+            "value": "GEN-UNIQUE"
+        },
+        "wlsLDAPPrincipal": {
+            "value": "GEN-UNIQUE"
+        },
+        "wlsLDAPPrincipalPassword": {
+            "value": "GEN-UNIQUE"
+        },
+        "wlsLDAPProviderName": {
+            "value": "AzureActiveDirectoryProvider"
+        },
+        "wlsLDAPSSLCertificate": {
+            "value": "GEN-UNIQUE"
+        },
+        "wlsLDAPUserBaseDN": {
+            "value": "GEN-UNIQUE"
+        },
+        "wlsPassword": {
+            "value": "GEN-UNIQUE"
+        },
+        "wlsUserName": {
+            "value": "GEN-UNIQUE"
+        }
+    }
+}
+EOF
