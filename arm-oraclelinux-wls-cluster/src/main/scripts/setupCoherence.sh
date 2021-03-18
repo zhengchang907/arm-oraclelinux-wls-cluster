6a9615e83a5a14038a7b3a37625f210990dcca6b
Gurudutt Suryanarayana <29789603+gnsuryan@users.noreply.github.com>
2021-03-19 02:32:51 +0530

    Further Fix for issue# 275 - configuring SSL keystores for Coherence Servers as well (#141)
    * fix for issue#293 keystore validation command failure
    
    * Fix for issue#294 - fix UI labels
    
    * Further Fix for issue# 275: Include SSL configuration for Coherence Cache server
    
    * correct minor issues
    
    Co-authored-by: gnsuryan <gnsuryan@users.noreply.github.com>

--- arm-oraclelinux-wls-cluster/src/main/scripts/setupCoherence.sh
+++ arm-oraclelinux-wls-cluster/src/main/scripts/setupCoherence.sh
@@ -202,6 +202,36 @@ topology:
            Notes: "$wlsServerName managed server"
            Cluster: "$storageClusterName"
            Machine: "$nmHost"
+EOF
+
+        if [ "${isCustomSSLEnabled}" == "true" ];
+        then
+cat <<EOF>>$wlsDomainPath/managed-domain.yaml
+           KeyStores: 'CustomIdentityAndCustomTrust'
+           CustomIdentityKeyStoreFileName: "$customIdentityKeyStoreFileName"
+           CustomIdentityKeyStoreType: "$customIdentityKeyStoreType"
+           CustomIdentityKeyStorePassPhraseEncrypted: "$customIdentityKeyStorePassPhrase"
+           CustomTrustKeyStoreFileName: "$customTrustKeyStoreFileName"
+           CustomTrustKeyStoreType: "$customTrustKeyStoreType"
+           CustomTrustKeyStorePassPhraseEncrypted: "$customTrustKeyStorePassPhrase"
+EOF
+        fi
+
+cat <<EOF>>$wlsDomainPath/managed-domain.yaml
+           SSL:
+                HostnameVerificationIgnored: true
+                HostnameVerifier: 'None'
+EOF
+
+        if [ "${isCustomSSLEnabled}" == "true" ];
+        then
+cat <<EOF>>$wlsDomainPath/managed-domain.yaml
+                ServerPrivateKeyAlias: "$serverPrivateKeyAlias"
+                ServerPrivateKeyPassPhraseEncrypted: "$serverPrivateKeyPassPhrase"
+EOF
+        fi
+
+cat <<EOF >>$wlsDomainPath/managed-domain.yaml
    SecurityConfiguration:
        NodeManagerUsername: "$wlsUserName"
        NodeManagerPasswordEncrypted: "$wlsPassword" 
@@ -233,6 +263,8 @@ EOF
 function create_ms_server_model() {
     echo "Creating managed server $wlsServerName model"
     cat <<EOF >$wlsDomainPath/add-server.py
+
+isCustomSSLEnabled='${isCustomSSLEnabled}'
 connect('$wlsUserName','$wlsPassword','t3://$wlsAdminURL')
 edit("$wlsServerName")
 startEdit()
@@ -244,10 +276,23 @@ cmo.setCluster(getMBean('/Clusters/$storageClusterName'))
 cmo.setListenAddress('$nmHost')
 cmo.setListenPort(int($storageListenPort))
 cmo.setListenPortEnabled(true)
+
+if isCustomSSLEnabled == 'true' :
+    cmo.setKeyStores('CustomIdentityAndCustomTrust')
+    cmo.setCustomIdentityKeyStoreFileName('$customIdentityKeyStoreFileName')
+    cmo.setCustomIdentityKeyStoreType('$customIdentityKeyStoreType')
+    set('CustomIdentityKeyStorePassPhrase', '$customIdentityKeyStorePassPhrase')
+    cmo.setCustomTrustKeyStoreFileName('$customTrustKeyStoreFileName')
+    cmo.setCustomTrustKeyStoreType('$customTrustKeyStoreType')
+    set('CustomTrustKeyStorePassPhrase', '$customTrustKeyStorePassPhrase')
+
 cd('/Servers/$wlsServerName/SSL/$wlsServerName')
-cmo.setEnabled(false)
-cd('/Servers/$wlsServerName//ServerStart/$wlsServerName')
-arguments = '-Dweblogic.Name=$wlsServerName  -Dweblogic.management.server=http://$wlsAdminURL ${wlsCoherenceArgs}'
+cmo.setServerPrivateKeyAlias('$serverPrivateKeyAlias')
+set('ServerPrivateKeyPassPhrase', '$serverPrivateKeyPassPhrase')
+cmo.setHostnameVerificationIgnored(true)
+
+cd('/Servers/$wlsServerName/ServerStart/$wlsServerName')
+arguments = '-Dweblogic.Name=$wlsServerName -Dweblogic.security.SSL.ignoreHostnameVerification=true -Dweblogic.management.server=http://$wlsAdminURL ${wlsCoherenceArgs}'
 cmo.setArguments(arguments)
 save()
 resolve()
@@ -678,8 +723,8 @@ else
     installUtilities
     mountFileShare
     openManagedServerPorts
-    createManagedSetup
     storeCustomSSLCerts
+    createManagedSetup
     createNodeManagerService
     enabledAndStartNodeManagerService
     startManagedServer
